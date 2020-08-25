2c22da4a666dd4b5f3fe681d818aa828424dcd77
ricardocascan <ricardocascan@gmail.com>
2020-08-25 13:55:02 +0200

    web

---
+++ WebApplication1/Models/Factura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Factura
+    {
+        public int Numero { get; set; }
+        public String Concepto { get; set; }
+
+        public Factura(int numero, string concepto)
+        {
+            Numero = numero;
+            Concepto = concepto;
+        }
+
+        public Factura()
+        {
+        }
+
+
+    }
+}
---
+++ WebApplication1/WebAPIController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class WebAPIController : ApiController
+    {
+
+        static List<Factura> lista = new List<Factura>();
+
+        static WebAPIController()
+        {
+            Factura f = new Factura(1, "ordenador");
+            Factura f2 = new Factura(2, "tablet");
+            lista.Add(f);
+            lista.Add(f2);
+        }
+
+        // GET api/<controller>
+        public IList<Factura> Get()
+        {          
+            return lista;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]Factura factura)
+        {
+            lista.Add(factura);
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
